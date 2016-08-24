69e489b309961294bf2f74d51d00725ad2069870
swapnilP <swapnil.p@amuratech.com>
2016-08-24 11:45:43 +0530

    Signup Created

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
   test "should get new" do
-    get :new
+    get :new                                      #get take action  not route
     assert_response :success
   end
 
