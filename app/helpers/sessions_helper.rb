da703c5547117d3072518852c5ea87c5a5110f0d
swapnilP <swapnil.p@amuratech.com>
2016-08-25 15:24:08 +0530

    logout added

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -9,10 +9,19 @@ module SessionsHelper
 
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
+    #because everytime call to current user shouldnt hit databse
+
   end
+
 
   def logged_in?
     !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:user_id)
+    #set current user to nil important
+    @current_user = nil
   end
 
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -8,7 +8,7 @@ class ActiveSupport::TestCase
 
   # Add more helper methods to be used by all tests here...
   #because methods in helper are not accessible here.
-  
+
   def is_logged_in?
     !session[:user_id].nil?
   end
