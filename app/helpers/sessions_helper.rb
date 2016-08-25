a7e139106c1f6d5631ca5e07a16f07fcf5bf2778
swapnilP <swapnil.p@amuratech.com>
2016-08-25 14:29:25 +0530

    temporary sessions added

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,15 @@ module SessionsHelper
     session[:user_id] = user.id
     #this session is session method of rails
     #not session hash in params
+    #treat it like a hash.
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
   end
 
 end
