456a7a24e5250f578af240118b0f4e530bc65d0e
swapnilP <swapnil.p@amuratech.com>
2016-09-09 18:26:26 +0530

    microposts added with css

--- db/seeds.rb
+++ db/seeds.rb
@@ -26,3 +26,12 @@ User.create!(name:  "Example User",
                activated: true,
                activated_at: Time.zone.now)
 end
+
+
+users = User.order(:created_at).take(6)
+#took first created so that their ids are known 1 to 6
+
+50.times do
+  content = Faker::Lorem.sentence(5)
+  users.each { |user| user.microposts.create!(content: content) }
+end
