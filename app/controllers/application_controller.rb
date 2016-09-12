c186901f370f99a717f3efd8afd504e544be8a8c
swapnilP <swapnil.p@amuratech.com>
2016-09-12 16:32:18 +0530

    micropost added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,14 @@ class ApplicationController < ActionController::Base
     render html: "hello world!"
   end
 
+  def logged_in_user
+
+      unless logged_in?      #checking only whether user logged in or not is not enough,
+                              #this only fix access to non logged users to edit and update
+        store_location
+        flash[:danger] = "Please log in."
+        redirect_to login_url
+      end
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,14 +14,15 @@ class UsersController < ApplicationController
      redirect_to(root_url) unless current_user?(@user)
   end
 
-  def logged_in_user
-      unless logged_in?      #checking only whether user logged in or not is not enough,
-                              #this only fix access to non logged users to edit and update
-        store_location
-        flash[:danger] = "Please log in."
-        redirect_to login_url
-      end
-  end
+  # def logged_in_user
+  #     unless logged_in?      #checking only whether user logged in or not is not enough,
+  #                             #this only fix access to non logged users to edit and update
+  #       store_location
+  #       flash[:danger] = "Please log in."
+  #       redirect_to login_url
+  #     end
+  # end
+  #Moved to ApplicationController
 
   def new
     @user=User.new
