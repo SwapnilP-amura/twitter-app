9288d522c4da8e211b73e0ecc0a6fc00f28a60a1
swapnilP <swapnil.p@amuratech.com>
2016-09-13 15:14:15 +0530

    view template

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,14 +3,23 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
 
   def setup
-   @user = users(:michael)
+    @user = users(:michael)
+    @other_user = users(:archer)
   end
-
   test "should get new" do
     get :new                                      #get take action  not route
     assert_response :success
   end
 
+  test "should redirect following when not logged in" do
+    get following_user_path(@user)
+    assert_redirected_to login_url
+  end
+
+  test "should redirect followers when not logged in" do
+    get followers_user_path(@user)
+    assert_redirected_to login_url
+  end
 
 
 
